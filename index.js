ef40018b63c9a34ce4207627854d72f59ff82335
filipi.fuchter <filipi.fuchter@digitro.com.br>
2018-04-12 09:19:06 -0300

    Implementando para leitura das informações do rádio

---
+++ index.js
@@ -0,0 +1,72 @@
+(function () {
+    "use strict";
+
+    const SerialPort = require('serialport');
+    const Readline = SerialPort.parsers.Readline;
+
+    class RadioGW {
+
+        constructor(){
+            this.serialPortConnected = false;
+        }
+
+        connect(usbPort){
+            this.usbPort = usbPort;
+            this.reconnect();
+        }
+
+        reconnect(){
+            this.port = new SerialPort(this.usbPort, {
+                baudRate: 2400
+            });
+            this.parser = this.port.pipe(new Readline({ delimiter: '\r\n' }));
+            this.addEventListeners();
+        }
+
+        addEventListeners(){
+            this.port.on('open', function() {
+              console.log('opened connection!');
+              this.serialPortConnected = true;
+            }.bind(this));
+
+            this.parser.on('data', (data) => {
+                this.onReceiveData(data);
+            });
+
+            //Tratamentos para caso de erro na conexão
+            this.port.on('close', (data) => {
+                console.error('Closed: ', data);
+                this.onError('Perdida conexão com a porta serial!');
+            });
+            this.port.on('error', function(err) {
+                console.error('Error: ', err.message);
+                this.onError(err.message);
+            });
+        }
+
+        onError(msg){
+            this.serialPortConnected = false;
+        }
+
+        onReceiveData(data){
+            console.log('Received =>', data);
+            //Não emite mais os eventos das respostas que contem caracteres inválidos
+            if(this.containsOnlyValidCharacters(data)){
+                //TODO fazer a validação se está autorizado
+            }else{
+                console.log('Desconsiderado dado por conter caracteres inválidos!');
+            }
+        }
+
+        containsOnlyValidCharacters(inputtxt) {
+          //When you have both ^ and $ you tell the engine that whatever is in between them must cover the entire line end-to-end
+          let lettersNumbersSpacesOnly = /^[0-9a-zA-Z-\s]+$/;
+          return lettersNumbersSpacesOnly.test(inputtxt);
+        }
+
+    }
+
+    let radioGW = new RadioGW();
+    radioGW.connect("/dev/ttyUSB0");
+
+})();
